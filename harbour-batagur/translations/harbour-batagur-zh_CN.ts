fa0d97f567a972073f7f3d5b557b7ed44289fd47
TylerTemp <tylertempdev@gmail.com>
2015-04-04 20:49:43 +0800

    add Chinese Simplified(zh_CN) translation

---
+++ harbour-batagur/translations/harbour-batagur-zh_CN.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="vanished">作者</translation>
+    </message>
+    <message>
+        <source>AuthorText</source>
+        <translation type="vanished">toerb (https://github.com/toerb/batagur/)</translation>
+    </message>
+    <message>
+        <source>Passwords</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>PasswordsText</source>
+        <translation>密码文本</translation>
+    </message>
+    <message>
+        <source>Licences</source>
+        <translation>许可协议</translation>
+    </message>
+    <message>
+        <source>LicencesText</source>
+        <translation>许可协议文本</translation>
+    </message>
+</context>
+<context>
+    <name>Input</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>Password</name>
+    <message>
+        <source>Copy to Clipboard</source>
+        <translation>复制到剪贴板</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <source>Clear Clipboard</source>
+        <translation>清空剪贴板</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Salt</source>
+        <translation>盐</translation>
+    </message>
+    <message>
+        <source>Iterations</source>
+        <translation>迭代次数</translation>
+    </message>
+    <message>
+        <source>Length</source>
+        <translation>长度</translation>
+    </message>
+</context>
+</TS>
