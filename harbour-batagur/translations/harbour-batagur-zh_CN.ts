92731d4a7baedc6d7d9eda30db3ee0ca27bef48a
TylerTemp <tylertempdev@gmail.com>
2015-04-06 20:23:48 +0800

    update Chinese Simplified(zh_CN) translation (About Page part)

--- harbour-batagur/translations/harbour-batagur-zh_CN.ts
+++ harbour-batagur/translations/harbour-batagur-zh_CN.ts
@@ -13,7 +13,10 @@
     </message>
     <message>
         <source>PasswordsText</source>
-        <translation>密码文本</translation>
+        <translation>&lt;p&gt;通过数学的单向函数，只需一个&quot;主密码&quot;就能生成无限多个新密码。你需要记住这个主密码。&lt;/p&gt;
+&lt;p&gt;为了生成不同的密码，需要进一步提供一些变量给单向函数：标记。&lt;/p&gt;
+&lt;p&gt;标记不需要保密。你应该从你将使用密码的服务那里获取标记。例如，如果你想给你的Jolla账号生成密码，你可以使用&quot;jolla&quot;作为密码。&lt;/p&gt;
+&lt;p&gt;想再次获得生成的密码，你只需使用相同的主密码和标记再生成一次即可——生成的密码是一样的。因此，不需要将密码记录到磁盘中。&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Licences</source>
@@ -21,11 +24,15 @@
     </message>
     <message>
         <source>LicencesText</source>
-        <translation>许可协议文本</translation>
+        <translation>&lt;p&gt;SHA512哈希库由Olivier Gay创作并在BDS协议下发布。
+网站: &lt;a href=&quot;http://www.ouah.org/ogay/sha2/&quot;&gt;http://www.ouah.org/ogay/sha2/&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;base91编码库来自Mario &apos;rlyeh&apos; Rodriguez和Joachim Henke。
+网站: &lt;a href=&quot;https://github.com/r-lyeh/base91&quot;&gt;https://github.com/r-lyeh/base91&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;该应用中我写的那部分代码在BSD协议下发布。&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Issues</source>
-        <translation type="unfinished"></translation>
+        <translation>问题和源码</translation>
     </message>
 </context>
 <context>
@@ -40,23 +47,23 @@
     </message>
     <message>
         <source>generate</source>
-        <translation type="unfinished"></translation>
+        <translation>生成</translation>
     </message>
     <message>
         <source>master password</source>
-        <translation type="unfinished"></translation>
+        <translation>主密码</translation>
     </message>
     <message>
         <source>master</source>
-        <translation type="unfinished"></translation>
+        <translation>主密码</translation>
     </message>
     <message>
         <source>service token</source>
-        <translation type="unfinished"></translation>
+        <translation>服务标记</translation>
     </message>
     <message>
         <source>token</source>
-        <translation type="unfinished"></translation>
+        <translation>标记</translation>
     </message>
 </context>
 <context>
